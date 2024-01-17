79157d8bdb0b300a8ad4ca836fd673d6a5d6acd1
Satyam Yendhe <49835853+satyamyendhe@users.noreply.github.com>
2024-01-17 14:12:13 +0530

    Update anagram.js

--- Assignments/Week-1/easy/anagram.js
+++ Assignments/Week-1/easy/anagram.js
@@ -4,13 +4,36 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 const string1 = "rail safety";
-const string2 = "fairy tales";
 function isAnagram(str1, str2) {
-  let string1 = str1.toLowerCase().split("").sort().join("");
-  let string2 = str2.toLowerCase().split("").sort().join("");
-  return string1 === string2;
+
+    if(str1.length == str2.length){
+        str1 = str1.toLowerCase().split("");
+        str2 = str2.toLowerCase().split("");
+
+        str1.sort();
+        str2.sort();
+        
+        // console.log(str1,str2)
+        let length = str1.length;
+        
+        for(let i = 0; i < length; i++){
+            if(str1[i] != str2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
+
 }
 
-console.log(isAnagram(string1, string2));
+if (isAnagram('abc!', '!bac')) {
+    console.log("String is anagram");
+}else {
+    console.log("String is not anagram");
+}
+
 
 module.exports = isAnagram;
